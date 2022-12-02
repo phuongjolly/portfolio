c165e98489b2373bc6ea2871ea99f7e7ea08e020
phuongjolly <lephuong06t3@gmail.com>
2022-12-02 00:01:05 -0800

    fix item box component style

--- src/common/ItemBox.js
+++ src/common/ItemBox.js
@@ -3,17 +3,19 @@ import "./ItemBox.css";
 
 export default function ItemBox({ data }) {
   return (
-    <div className="item-box flex flex-col" key={data._id}>
+    <div className="item-box flex flex-col justify-between" key={data._id}>
       <Link to={`/showcase/${data._id}`}>
         <img className={"w-full"} src={data.avatar} alt="myBlog" />
       </Link>
-      <div className="font-semibold text-2xl">{data.title}</div>
-      <div className={"flex flex-row justify-between"}>
-        <ul className={"flex flex-row gap-4 text-base font-semibold"}>
-          <li>{data.date}</li>
-          <li>{data.minRead} minutes read</li>
-        </ul>
-        <div>{data.comment} comments</div>
+      <div className={"flex flex-col justify-between h-full"}>
+        <div className="font-semibold text-2xl">{data.title}</div>
+        <div className={"flex flex-row justify-between items-center"}>
+          <ul className={"flex flex-row gap-4 text-base font-semibold"}>
+            <li>{data.date}</li>
+            <li>{data.minRead} minutes read</li>
+          </ul>
+          <div>{data.comment} comments</div>
+        </div>
       </div>
     </div>
   );
